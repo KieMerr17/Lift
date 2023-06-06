a1d25cad5600d4fa4e45f90b9ef56ac16b7d6be0
KieMerr17 <kieran.merrett@hotmail.com>
2023-06-06 11:44:41 +0000

    add images to switch between using javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
-function main(){
-    console.log("Hello")
-}
+document.getElementById('lift_open').addEventListener('click', function() {
+    document.getElementById('lift_open_img').style.display = 'block';
+    document.getElementById('lift_closed_img').style.display = 'none';
+});
 
+document.getElementById('lift_closed').addEventListener('click', function() {
+    document.getElementById('lift_open_img').style.display = 'none';
+    document.getElementById('lift_closed_img').style.display = 'block';
+});
 
-main()
+document.getElementById('lift_environ_1').addEventListener('click', function() {
+    document.getElementById('lift_environ_img_2').style.display = 'none';
+    document.getElementById('lift_environ_img_1').style.display = 'block';
+});
+
+document.getElementById('lift_environ_2').addEventListener('click', function() {
+    document.getElementById('lift_environ_img_1').style.display = 'none';
+    document.getElementById('lift_environ_img_2').style.display = 'block';
+});
